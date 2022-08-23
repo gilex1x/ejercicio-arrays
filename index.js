1ab97d31e0e7f758877bd54eb365e8593ce7b471
Gilberto Santamaría <gilbertosa099@outlook.com>
2022-08-22 19:45:53 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const pairNumbersArray = () => {
+  let arr = [];
+  for (let i = 2; arr.length < 10; i++) {
+    i % 2 == 0 && arr.push(i);
+  }
+  console.log(arr);
+};
+
+// const createMatriz = () => {
+//   /*Primero creamos la matriz, a diferencia de otros lenguajes,
+//     en javascrip para crear una Matriz, creamos un array dentro de cada una
+//     de las posiciones de otro Array*/
+//   let matriz = [];
+//   for (var i = 0; i < 5; i++) {
+//     matriz[i] = new Array(5);
+//   }
+//   return matriz;
+// };
+
+const fillMatriz = () => {
+  let matrizToFill = [
+    [, , , ,],
+    [, , , ,],
+    [, , , ,],
+    [, , , ,],
+    [, , , ,],
+  ];
+  for (let i = 0; i < matrizToFill.length * 5; i++) {
+    //debugger;
+    let rowIndex = parseInt(i / 5, 10);
+    //debugger;
+    let columnIndex = i % 5;
+    //debugger;
+    matrizToFill[rowIndex][columnIndex] = `${rowIndex + 1},${columnIndex + 1}`;
+    //debugger;
+  }
+  console.log(...matrizToFill);
+};
+
+let values = [];
+const captureUserInput = (event) => {
+  let userInput = event.target.value;
+  values = userInput.split(",");
+  if (values.some((item) => item > 10)) {
+    alert("Alguno de los valores es mayor a 10, por favor rectifica");
+    event.target.value = "";
+    values = [];
+  }
+};
+
+const fillUserMatriz = () => {
+  console.log(values);
+  let matrizToFill = [
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+  ];
+  for (i = 0; i < 3; i++) {
+    matrizToFill[i].forEach((item, index) => {
+      index < values[i] && (matrizToFill[i][index] = "*");
+    });
+  }
+  console.log(matrizToFill);
+};
